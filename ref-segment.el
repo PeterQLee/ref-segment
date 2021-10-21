015e1c6d6287ebe1030cbdb1cf6c002950bade24
PeterQLee <leep1995@gmail.com>
2021-10-21 12:52:52 -0400

    add update commands

--- ref-segment.el
+++ ref-segment.el
@@ -1,7 +1,7 @@
 
 (defun ref-template (title path author subject keywords url)
   "Org-mode template for adding a new article"
-  (format "* %s\n** [path]\n[[%s][%s]]\n** tags\n:@tag::@tag:\n** [Isread]\n** [Author]\n%s\n** [subject]\n%s\n** [keywords]\n%s\n** [url]\n%s\n** [notes]\n" title path path author subject keywords url)
+  (format "* %s\n** [path]\n[[%s][%s]]\n** tags\n:@tag::@tag:\n** [Isread]\n** [Author]\n%s\n** [subject]\n%s\n** [keywords]\n%s\n** [url]\n%s\n** [notes]\n** [reference]\n" title path path author subject keywords url)
   )
 
 
@@ -128,3 +128,39 @@
    (org-mode)
 
  ))
+
+
+(defun add-log-to-db (start end)
+  "Adds updated info to the database"
+  (interactive "r")
+  (setq contents (buffer-substring-no-properties start end))
+  (setq subject (read-string "subject:"))
+  (princ (call-process-region start end "pap_org_convert" nil nil nil subject))
+  )
+
+(defun gen-paper-log-from-db ()
+  "Generates a list of entries from the database"
+  (interactive)
+  (setq subject (read-string "subject:"))
+  (setq result (shell-command-to-string (format "pap_gen_org %s" subject)))
+
+  (with-output-to-temp-buffer (format "*%s*" subject)
+    (princ result))
+  (org-mode)
+  )
+
+(defun open-article-in-okular ()
+  "opens the article into okular"
+  (interactive)
+  (setq buf (org-element-context))
+  (setq pdfpath (plist-get (car (cdr buf)) :path))
+  (call-process-shell-command (format "okular %s &" pdfpath))
+  )
+  
+			      
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "C-c C-1") 'add-log-to-db)
+	    (local-set-key (kbd "C-c C-2") 'gen-paper-log-from-db)
+	    (local-set-key (kbd "C-c C-3") 'open-article-in-okular)
+	    ))
